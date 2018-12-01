c07a48a9b052f3bfba0d0e7a2308b2c19af4905f
joshsarna <joshsarna@gmail.com>
2018-11-30 18:16:50 -0800

    customize event post type with labels

--- university-post-types.php
+++ university-post-types.php
@@ -4,12 +4,17 @@
     register_post_type('event', array(
       'public' => true,
       'labels' => array(
-        'name' => 'Events'
+        'name' => 'Events',
+        'add_new_item' => 'Add New Event',
+        'edit_item' => 'Edit Event',
+        'all_items' => 'All Events',
+        'singular_name' => 'Event',
+        'search_items' => 'Search Events'
       ),
       'menu_icon' => 'dashicons-calendar'
     ));
   }
 
   add_action('init', 'university_post_types');
-  
+
 ?>
