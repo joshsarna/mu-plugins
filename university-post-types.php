23d59d1a245c5e08d69f8510dba8e617859d009a
joshsarna <joshsarna@gmail.com>
2018-12-06 14:47:05 -0800

    add campus post type

--- university-post-types.php
+++ university-post-types.php
@@ -49,6 +49,23 @@
       ),
       'menu_icon' => 'dashicons-welcome-learn-more'
     ));
+
+    // Campus Post Type
+    register_post_type('campus', array(
+      'supports' => array('title', 'editor', 'excerpt'),
+      'rewrite' => array('slug' => 'campuses'),
+      'has_archive' => true,
+      'public' => true,
+      'labels' => array(
+        'name' => 'Campuses',
+        'add_new_item' => 'Add New Campus',
+        'edit_item' => 'Edit Campus',
+        'all_items' => 'All Campuses',
+        'singular_name' => 'Campus',
+        'search_items' => 'Search Campuses'
+      ),
+      'menu_icon' => 'dashicons-location-alt'
+    ));
   }
 
   add_action('init', 'university_post_types');
